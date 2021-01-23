f330deccfcfe126cfe7e6f6ca6f0e31e3d14a3f7
pantherZh <52707302+pantherZh@users.noreply.github.com>
2021-01-23 17:23:49 +0300

    Add files via upload

---
+++ TheBestDollarRate/ClassLibrary/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ClassLibrary
+{
+    public class Bank : IComparable
+    {
+        public object locker = new object();
+        public string Name { get { return this.name;  } private set { name = value; } }
+        private string name;
+
+        public string Address { get { return this.address;  } private set { address = value; } }
+        private string address;
+
+        public decimal USD_in { get { lock (locker) { return this.usd_id; } } private set { usd_id = value; } }
+        private decimal usd_id;
+
+        public decimal USD_out { get { lock (locker) { return this.usd_out; } } private set { usd_out = value; } }
+        private decimal usd_out;
+
+
+        public Bank(string name, string address, string expression, int group1, int group2)
+        {
+            Name = name;
+            Address = address;
+
+            new Task(() =>
+            {
+                try
+                {
+                    lock (locker)
+                    {
+                        HttpWebRequest request = HttpWebRequest.CreateHttp(Address);
+                        request.UserAgent = "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 70.0.3538.102 Safari / 537.36 Edge / 18.19041";
+
+                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                        StreamReader reader = new StreamReader(response.GetResponseStream());
+                        Match match = new Regex(expression, RegexOptions.Compiled | RegexOptions.Singleline).Match(reader.ReadToEnd().ToString());
+                        reader.Close();
+
+                        USD_in = decimal.Parse((match.Groups[group1].Value).Replace(".", ","));
+                        USD_out = decimal.Parse((match.Groups[group2].Value).Replace(".", ","));
+                    }
+                }
+                catch (WebException ex)
+                {
+                    WebExceptionStatus status = ex.Status;
+
+                    if (status == WebExceptionStatus.ProtocolError)
+                    {
+                        HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                        Console.WriteLine("Статусный код ошибки: {0} - {1}",
+                                (int)httpResponse.StatusCode, httpResponse.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }).Start();
+        }
+        public Bank(string name, string address, string expression, int group1, int group2, bool sync)
+        {
+            Name = name;
+            Address = address;
+
+            HttpWebRequest request = HttpWebRequest.CreateHttp(Address);
+            request.UserAgent = "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 70.0.3538.102 Safari / 537.36 Edge / 18.19041";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            Match match = new Regex(expression, RegexOptions.Compiled | RegexOptions.Singleline).Match(reader.ReadToEnd().ToString());
+            reader.Close();
+
+            this.USD_in = decimal.Parse((match.Groups[group1].Value).Replace(".", ","));
+            this.USD_out = decimal.Parse((match.Groups[group2].Value).Replace(".", ","));
+        }
+
+        public int CompareTo(object obj)
+        { 
+            Bank b = (Bank)obj;
+            return Decimal.Compare(this.USD_out, b.USD_out);
+        }
+
+        private class SortUsdInDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                Bank b1 = (Bank)x;
+                Bank b2 = (Bank)y;
+                return Decimal.Compare(b2.USD_in, b1.USD_in);
+            }
+        }
+        public static IComparer SortUsdInDescending()
+        {
+            return (IComparer)new SortUsdInDescendingHelper();
+        }
+    }
+    
+}
+
+
+
+
---
+++ TheBestDollarRate/TheBestDR/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using ClassLibrary;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Xml;
+using System.Diagnostics;
+
+namespace std
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Bank nb = new Bank("Nb rb", "https://www.nbrb.by/api/exrates/rates/145", @"""Cur_OfficialRate"":(.*?)}", 1, 1, true);
+            Console.WriteLine($"\t\t-----Курс национального банка РБ: {nb.USD_in}-----");
+
+            
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"C:\Users\alesy\source\repos\TheBestDollarRate\tasks.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            Bank[] banks = new Bank[xRoot.ChildNodes.Count];
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            Console.WriteLine("Start, всего банков " + xRoot.ChildNodes.Count);
+
+
+
+            banks = StartParallelFor(xRoot);
+            //banks = StartFor(xRoot);
+            int i = 0;
+            foreach (Bank b in banks)
+            {
+                Console.WriteLine($"{i}:{b.Name} -> {b.USD_in}/{b.USD_out}");
+                i++;
+            }
+
+            Console.WriteLine("--------Покупка--------");
+            Array.Sort(banks, Bank.SortUsdInDescending());
+            foreach (Bank b in banks)
+            {
+                Console.WriteLine($"{b.USD_in} - {b.Name}");
+            }
+            Array.Sort(banks);
+            Console.WriteLine("--------Продажа--------");
+            foreach (Bank b in banks)
+            {
+                Console.WriteLine($"{b.USD_out} - {b.Name}");
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("End tasks, elapsed time(ms) ->" + stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("\n\n\n");
+        }
+
+        public static Bank[] StartFor(XmlElement xRoot)
+        {
+            List<Bank> banks = new List<Bank>();
+            string name = "", expression = "", address = "";
+            int group1 = 0, group2 = 0; 
+
+            foreach (XmlElement xnode in xRoot)
+            {
+                XmlNode attr = xnode.Attributes.GetNamedItem("name");
+
+                if (attr != null)
+                    name = attr.Value;
+
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "address")
+                        address = childnode.InnerText;
+                    if (childnode.Name == "expression")
+                        expression = childnode.InnerText;
+                    if (childnode.Name == "group1")
+                        group1 = int.Parse(childnode.InnerText);
+                    if (childnode.Name == "group2")
+                        group2 = int.Parse(childnode.InnerText);
+                }
+
+                banks.Add(new Bank(name, address, expression, group1, group2));
+            }
+            return banks.ToArray();
+        }
+
+
+        public static Bank[] StartParallelFor(XmlElement xRoot)
+        {
+            List<Bank> banks = new List<Bank>();
+            object locker = new object();
+            string name = "", expression = "", address = "";
+            int group1 = 0, group2 = 0;
+
+            ParallelLoopResult result = Parallel.For(0, xRoot.ChildNodes.Count, _ =>
+            {
+                
+                lock (locker)
+                {
+                    Console.WriteLine("Добавление банка " + _);
+                    name = xRoot.ChildNodes[_].Attributes["name"].Value;
+                
+                    foreach (XmlNode childnode in xRoot.ChildNodes[_].ChildNodes)
+                    {
+                        if (childnode.Name == "address")
+                            address = childnode.InnerText;
+                        if (childnode.Name == "expression")
+                            expression = childnode.InnerText;
+                        if (childnode.Name == "group1")
+                            group1 = int.Parse(childnode.InnerText);
+                        if (childnode.Name == "group2")
+                            group2 = int.Parse(childnode.InnerText);
+                    } 
+                }
+                banks.Add(new Bank(name, address, expression, group1, group2));
+            });
+
+            return banks.ToArray();
+        }
+    }
+}
